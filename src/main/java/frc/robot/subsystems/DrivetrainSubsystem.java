619de2e7540d0702e07f0a41bde32ebfefd4a58e
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2023-11-01 20:10:59 -0700

    intital drive train commit

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  /** Creates a new DrivetrainSubsystem. */
+  public CANSparkMax m_leftMotor1;
+  public CANSparkMax m_leftMotor2;
+  public CANSparkMax m_rightMotor1;
+  public CANSparkMax m_rightMotor2;
+  public DrivetrainSubsystem() {
+    m_leftMotor1 = new CANSparkMax(Constants.LEFT_MOTOR1, MotorType.kBrushless);
+    m_leftMotor2 = new CANSparkMax(Constants.LEFT_MOTOR2, MotorType.kBrushless);
+    m_rightMotor1 = new CANSparkMax(Constants.RIGHT_MOTOR1, MotorType.kBrushless);
+    m_rightMotor2 = new CANSparkMax(Constants.RIGHT_MOTOR2, MotorType.kBrushless);
+
+    m_leftMotor1.setInverted(true);
+    m_leftMotor2.setInverted(true);
+
+  }
+
+  public void drive (double rightJoystick, double leftJoystick) {
+
+    m_leftMotor1.set(leftJoystick);
+    m_leftMotor2.set(leftJoystick);
+    m_rightMotor1.set(rightJoystick);
+    m_rightMotor2.set(rightJoystick);
+    System.out.println("drive method");
+  }
+  
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per schedule r run
+  }
+}
